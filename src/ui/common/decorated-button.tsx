1084d1494c8ba80f4ece53bbdf4b0cb182fb9245
Quaffel <niklas.radomski@sap.com>
2024-04-28 12:39:13 +0200

    Make decorated buttons selectable

--- src/ui/common/decorated-button.tsx
+++ src/ui/common/decorated-button.tsx
@@ -3,14 +3,26 @@ import { Resource, getResourcePath } from '../resources';
 import './decorated-button.css';
 
 export function DecoratedButton({
-    label, icon, disabled, onSelect
+    label,
+    icon,
+    disabled,
+    selected,
+    onSelect
 }: {
     label: string,
     icon: Resource,
     disabled?: boolean,
+    selected?: boolean,
     onSelect?: () => void,
-}): JSX.Element {
-    return <button className='decorated-button' type='button' disabled={disabled} onClick={() => onSelect?.()}>
+    }): JSX.Element {
+    const classes = ['decorated-button'];
+    if (selected) classes.push('selected');
+
+    return <button
+        className={classes.join(" ")}
+        type='button'
+        disabled={disabled || selected}
+        onClick={() => onSelect?.()}>
         {/* Buttons are replaced elements, meaning that CSS layout rules do not apply.  
             As buttons center their contents by default, we instead use a wrapping 'div' element of
             equal size and do the layouting within the inner 'div' element. */}
